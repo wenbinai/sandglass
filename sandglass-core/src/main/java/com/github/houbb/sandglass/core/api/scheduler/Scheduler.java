f9abc8264ef4e0200182adb02aa262bddac7f47f
houbb <1060732496@qq.com>
2020-04-06 23:33:17 +0800

    [Feature] add for new

--- sandglass-core/src/main/java/com/github/houbb/sandglass/core/api/scheduler/Scheduler.java
+++ sandglass-core/src/main/java/com/github/houbb/sandglass/core/api/scheduler/Scheduler.java
@@ -3,18 +3,18 @@ package com.github.houbb.sandglass.core.api.scheduler;
 import com.github.houbb.heaven.annotation.ThreadSafe;
 import com.github.houbb.sandglass.api.api.IJob;
 import com.github.houbb.sandglass.api.api.IScheduler;
+import com.github.houbb.sandglass.api.api.ISchedulerContext;
 import com.github.houbb.sandglass.api.api.ITrigger;
 
 /**
  * @author binbin.hou
- * @since 1.0.0
+ * @since 0.0.1
  */
 @ThreadSafe
 public class Scheduler implements IScheduler {
 
     @Override
-    public void schedule(IJob job, ITrigger trigger) {
-        //
+    public void schedule(ISchedulerContext context) {
     }
 
 }
