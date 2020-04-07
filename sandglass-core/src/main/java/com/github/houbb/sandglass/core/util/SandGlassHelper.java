06144cd2850e75742876e560a16c8f7b1414c1ca
binbin.hou <binbin.hou@huifu.com>
2020-04-07 15:19:26 +0800

    [Feature] add for new

--- sandglass-core/src/main/java/com/github/houbb/sandglass/core/util/SandGlassHelper.java
+++ sandglass-core/src/main/java/com/github/houbb/sandglass/core/util/SandGlassHelper.java
@@ -12,11 +12,6 @@ public final class SandGlassHelper {
 
     private SandGlassHelper(){}
 
-    //1. 核心的代码：job
-    // 默认开始时间：now==》触发时机
-    // 默认执行次数：3次==>停止策略
-    // 默认间隔时间：5s==>等待策略
-    // 默认任务调度器：最简单的单线程==》线程池
     /**
      * 执行任务
      * @since 0.0.1
